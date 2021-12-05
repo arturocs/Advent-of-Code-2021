3eb103b4e417fddce024dec3ab3906ed4475a00c
Arturo <arturocs@protonmail.com>
2021-12-05 13:43:19 +0100

    Use nalgebra for a better representation of the boards

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,28 +1,26 @@
-fn sum_unmarked_numbers(board: &[Vec<(i32, bool)>]) -> i32 {
+use nalgebra::Matrix5;
+
+fn sum_unmarked_numbers(board: &Matrix5<(i32, bool)>) -> i32 {
     board
         .iter()
-        .flatten()
-        .filter_map(|&(n, b)| if !b { Some(n) } else { None }).inspect(|i|println!("{}",i))
+        .filter_map(|&(n, b)| if !b { Some(n) } else { None })
         .sum()
 }
 
-fn check_boards(boards: &[Vec<Vec<(i32, bool)>>]) -> Option<(i32, usize)> {
-    for (n, board) in boards.iter().enumerate() {
-        if board.iter().find(|row| row.iter().all(|i| i.1)).is_some() {
-            return Some((sum_unmarked_numbers(board), n));
-        }
-        if (0..board.len())
-            .find(|&i| (0..board.len()).all(|j| board[j][i].1))
-            .is_some()
+fn check_boards(boards: &[Matrix5<(i32, bool)>]) -> Option<(i32, usize)> {
+    boards.iter().enumerate().find_map(|(n, board)| {
+        if board.row_iter().any(|row| row.iter().all(|i| i.1))
+            || board.column_iter().any(|col| col.iter().all(|i| i.1))
         {
-            return Some((sum_unmarked_numbers(board), n));
+            Some((sum_unmarked_numbers(board), n))
+        } else {
+            None
         }
-    }
-    None
+    })
 }
 
-fn update_boards(boards: &mut [Vec<Vec<(i32, bool)>>], n: i32) {
-    boards.iter_mut().flatten().flatten().for_each(|(i, b)| {
+fn update_boards(boards: &mut [Matrix5<(i32, bool)>], n: i32) {
+    boards.iter_mut().flatten().for_each(|(i, b)| {
         if *i == n {
             *b = true
         }
@@ -31,43 +29,28 @@ fn update_boards(boards: &mut [Vec<Vec<(i32, bool)>>], n: i32) {
 
 fn main() {
     let mut input = include_str!("../input").lines();
+    let draw_numbers = input.next().unwrap().split(',').map(|i| i.parse().unwrap());
 
-    let draw_numbers: Vec<i32> = input
-        .next()
-        .unwrap()
-        .split(',')
-        .map(|i| i.parse().unwrap())
-        .collect();
-
-    let mut boards: Vec<Vec<Vec<(i32, bool)>>> = input
-        .filter(|l| !l.is_empty())
-        .map(|l| {
-            l.split_whitespace()
-                .map(|i| (i.parse().unwrap(), false))
-                .collect()
-        })
+    let mut boards: Vec<Matrix5<(i32, bool)>> = input
+        .flat_map(|l| l.split_whitespace().map(|i| (i.parse().unwrap(), false)))
         .collect::<Vec<_>>()
-        .chunks(5)
-        .map(|i| i.to_vec())
+        .chunks(25)
+        .map(|i| Matrix5::from_column_slice(i))
         .collect();
 
-
-    dbg!(draw_numbers.len());
     let results: Vec<_> = draw_numbers
-        .into_iter()
         .filter_map(|n| {
             update_boards(&mut boards, n);
             check_boards(&boards).map(|(sum, i)| {
-                boards[i] = vec![vec![(-1,false);5];5];
-                //dbg!(i,n);
+                boards[i] = Matrix5::repeat((-1, false));
                 sum * n
             })
         })
-        //.filter(|&i|i!=0)
+        .filter(|&i| i != 0)
         .collect();
 
     dbg!(&results);
     dbg!(&results.len());
-    //println!("Part one: {}", results.first().unwrap());
+    println!("Part one: {}", results.first().unwrap());
     println!("Part two: {}", results.last().unwrap());
 }
