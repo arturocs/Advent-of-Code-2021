0cc4ee56a181ac5498db04fcb066957a3343d536
Arturo <arturocs@protonmail.com>
2021-12-07 20:03:37 +0100

    Added day 7

--- README.md
+++ README.md
@@ -6,4 +6,5 @@
 * [Day 4: Giant Squid](./day_4)
 * [Day 5: Hydrothermal Venture](./day_5)
 * [Day 6: Lanternfish](./day_6)
+* [Day 7: The Treachery of Whales](./day_7)
 
---
+++ day_7/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let input: Vec<i32> = include_str!("../input")
+        .split(',')
+        .map(|i| i.trim().parse().unwrap())
+        .collect();
+
+    let min = *input.iter().min().unwrap();
+    let max = *input.iter().max().unwrap();
+
+    let result_1: i32 = (min..max)
+        .map(|i| input.iter().map(|&j| (i - j).abs()).sum())
+        .min()
+        .unwrap();
+
+    let result_2: i32 = (min..max)
+        .map(|i| input.iter().map(|&j| (0..=(i - j).abs())).flatten().sum())
+        .min()
+        .unwrap();
+
+    println!("Part One: {}", result_1);
+    println!("Part Two: {}", result_2);
+}
